07a330bcdf9183b39556701ad117d8ae534133db
Olivier Breuleux <breuleux@gmail.com>
2025-05-28 16:56:26 -0400

    Add variable analysis

---
+++ src/funbite/tools.py
@@ -0,0 +1,123 @@
+import ast
+import dataclasses
+from dataclasses import dataclass, field
+
+from ovld import recurse
+
+from .visit import NodeVisitor
+
+
+@dataclass
+class Variables:
+    arg_defs: set[str] = field(default_factory=set)
+    local_defs: set[str] = field(default_factory=set)
+    uses_free: set[str] = field(default_factory=set)
+    uses_local: set[str] = field(default_factory=set)
+    nonlocals: set[str] = field(default_factory=set)
+    globals: set[str] = field(default_factory=set)
+
+    @property
+    def defs(self):
+        return self.arg_defs | self.local_defs
+
+    def use(self, varname):
+        if varname in self.defs:
+            self.uses_local.add(varname)
+        else:
+            self.uses_free.add(varname)
+
+    def define(self, varname):
+        if varname in self.nonlocals or varname in self.globals or varname in self.arg_defs:
+            return
+        self.local_defs.add(varname)
+        if varname in self.uses_free:
+            self.uses_free.remove(varname)
+            self.uses_local.add(varname)
+
+    def define_argument(self, varname):
+        if varname in self.nonlocals or varname in self.globals:
+            return
+        self.arg_defs.add(varname)
+        if varname in self.uses_free:
+            self.uses_free.remove(varname)
+            self.uses_local.add(varname)
+
+    def declare_nonlocal(self, varname):
+        if varname in self.local_defs:
+            self.local_defs.remove(varname)
+        self.nonlocals.add(varname)
+        if varname in self.uses_local:
+            self.uses_local.remove(varname)
+            self.uses_free.add(varname)
+
+    def declare_global(self, varname):
+        if varname in self.local_defs:
+            self.local_defs.remove(varname)
+        self.globals.add(varname)
+        if varname in self.uses_local:
+            self.uses_local.remove(varname)
+            self.uses_free.add(varname)
+
+    def clone(self):
+        return type(self)(
+            arg_defs=set(self.arg_defs),
+            local_defs=set(self.local_defs),
+            uses_free=set(self.uses_free),
+            uses_local=set(self.uses_local),
+            nonlocals=set(self.nonlocals),
+            globals=set(self.globals),
+        )
+
+    replace = dataclasses.replace
+
+
+class VariableAnalysis(NodeVisitor):
+    def inner(self, node: ast.FunctionDef, context: Variables):
+        for arg in node.args.args:
+            context.define_argument(arg.arg)
+        for arg in node.args.kwonlyargs:
+            context.define(arg.arg)
+        if node.args.vararg:
+            context.define(node.args.vararg.arg)
+        if node.args.kwarg:
+            context.define(node.args.kwarg.arg)
+        self(node.body, context)
+        return context
+
+    def __call__(self, node: ast.Name, context: Variables):
+        match node.ctx:
+            case ast.Load():
+                context.use(node.id)
+            case ast.Store():
+                context.define(node.id)
+                context.use(node.id)
+            case _:
+                raise Exception(f"Unsupported context: {node.ctx}")
+        return context
+
+    def __call__(self, node: ast.Nonlocal, context: Variables):
+        for name in node.names:
+            context.declare_nonlocal(name)
+        return context
+
+    def __call__(self, node: ast.Global, context: Variables):
+        for name in node.names:
+            context.declare_global(name)
+        return context
+
+    def __call__(self, node: ast.FunctionDef, context: Variables):
+        context.define(node.name)
+        inner_context = self.inner(node, Variables())
+        for expr in node.args.defaults:
+            recurse(expr, context)
+        for var in inner_context.uses_free:
+            if var in inner_context.globals:
+                context.uses_free.add(var)
+            else:
+                context.use(var)
+        return context
+
+    def reduce(self, node, results, context):
+        for _, x in results:
+            if isinstance(x, Variables):
+                return x
---
+++ tests/test_tools.py
@@ -0,0 +1,119 @@
+import ast
+import inspect
+import textwrap
+
+from funbite.tools import VariableAnalysis, Variables, split
+
+
+def test_varanal_local_variables():
+    code = textwrap.dedent("""
+    x = 1
+    y = x + 2
+    z = y
+    """)
+    tree = ast.parse(code)
+    results = VariableAnalysis.run(tree, context=Variables())
+
+    assert results == Variables(
+        local_defs={"x", "y", "z"},
+        uses_local={"x", "y", "z"},
+    )
+
+
+def test_varanal_free_variables():
+    code = "a = b + 1"
+    tree = ast.parse(code)
+    results = VariableAnalysis.run(tree, context=Variables())
+
+    assert results == Variables(
+        local_defs={"a"},
+        uses_local={"a"},
+        uses_free={"b"},
+    )
+
+
+def test_varanal_def():
+    def f(x, y):
+        z = x * y
+        return z + a
+
+    tree = ast.parse(textwrap.dedent(inspect.getsource(f)))
+    results = VariableAnalysis.run(tree, context=Variables())
+
+    assert results == Variables(
+        local_defs={"f"},
+        uses_free={"a"},
+    )
+
+
+def test_varanal_inner_def():
+    def f(x, y):
+        def g(z):
+            return z + x + a
+
+        return g(x * y * q)
+
+    tree = ast.parse(textwrap.dedent(inspect.getsource(f)))
+    inner_results = VariableAnalysis().inner(tree.body[0], context=Variables())
+    assert inner_results == Variables(
+        arg_defs={"x", "y"},
+        local_defs={"g"},
+        uses_local={"g", "x", "y"},
+        uses_free={"q", "a"},
+    )
+
+    outer_results = VariableAnalysis.run(tree, context=Variables())
+    assert outer_results == Variables(
+        local_defs={"f"},
+        uses_free={"q", "a"},
+    )
+
+
+def test_varanal_for():
+    def f():
+        z = 0
+        for i, q in enumerate(range(4, 9)):
+            z += q * i
+        return z
+
+    tree = ast.parse(textwrap.dedent(inspect.getsource(f)))
+    results = VariableAnalysis.run(tree, context=Variables())
+
+    assert results == Variables(
+        local_defs={"f"},
+        uses_free={"enumerate", "range"},
+    )
+
+
+def test_varanal_augass():
+    code = "a += b"
+    tree = ast.parse(code)
+    results = VariableAnalysis.run(tree, context=Variables())
+
+    assert results == Variables(
+        local_defs={"a"},
+        uses_local={"a"},
+        uses_free={"b"},
+    )
+
+
+def test_varanal_nonlocal():
+    code = "nonlocal x; x = 3;"
+    tree = ast.parse(code)
+    results = VariableAnalysis.run(tree, context=Variables())
+
+    assert results == Variables(
+        nonlocals={"x"},
+        uses_free={"x"},
+    )
+
+
+def test_varanal_global():
+    code = "global x; x = 3;"
+    tree = ast.parse(code)
+    results = VariableAnalysis.run(tree, context=Variables())
+
+    assert results == Variables(
+        globals={"x"},
+        uses_free={"x"},
+    )
