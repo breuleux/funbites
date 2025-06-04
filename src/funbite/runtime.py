7cd8c85fb020e1d55203d80781191eabb86b6055
Olivier Breuleux <breuleux@gmail.com>
2025-06-04 13:42:43 -0400

    Minor reorg

--- src/funbite/runtime.py
+++ src/funbite/runtime.py
@@ -1,17 +1,5 @@
 from functools import partial
 
 
-class FunBites:
-    def __init__(self, start, definitions):
-        self.start = start
-        self.definitions = definitions
-
-    def __call__(self, *args, **kwargs):
-        result = self.definitions[self.start](*args, **kwargs)
-        while isinstance(result, Bite):
-            result = result()
-        return result
-
-
-class Bite(partial):
+class FunBite(partial):
     pass
--- src/funbite/split.py
+++ src/funbite/split.py
@@ -9,125 +9,9 @@ from typing import Callable
 
 from ovld import call_next, ovld, recurse
 
-from .runtime import Bite
+from .runtime import FunBite
+from .vars import VariableAnalysis, Variables
 from .visit import NodeTransformer, NodeVisitor
-
-
-@dataclass
-class Variables:
-    arg_defs: set[str] = field(default_factory=set)
-    local_defs: set[str] = field(default_factory=set)
-    uses_free: set[str] = field(default_factory=set)
-    uses_local: set[str] = field(default_factory=set)
-    nonlocals: set[str] = field(default_factory=set)
-    globals: set[str] = field(default_factory=set)
-
-    @property
-    def defs(self):
-        return self.arg_defs | self.local_defs
-
-    def use(self, varname):
-        if varname in self.defs:
-            self.uses_local.add(varname)
-        else:
-            self.uses_free.add(varname)
-
-    def define(self, varname):
-        if varname in self.nonlocals or varname in self.globals or varname in self.arg_defs:
-            return
-        self.local_defs.add(varname)
-        if varname in self.uses_free:
-            self.uses_free.remove(varname)
-            self.uses_local.add(varname)
-
-    def define_argument(self, varname):
-        if varname in self.nonlocals or varname in self.globals:
-            return
-        self.arg_defs.add(varname)
-        if varname in self.uses_free:
-            self.uses_free.remove(varname)
-            self.uses_local.add(varname)
-
-    def declare_nonlocal(self, varname):
-        if varname in self.local_defs:
-            self.local_defs.remove(varname)
-        self.nonlocals.add(varname)
-        if varname in self.uses_local:
-            self.uses_local.remove(varname)
-            self.uses_free.add(varname)
-
-    def declare_global(self, varname):
-        if varname in self.local_defs:
-            self.local_defs.remove(varname)
-        self.globals.add(varname)
-        if varname in self.uses_local:
-            self.uses_local.remove(varname)
-            self.uses_free.add(varname)
-
-    def clone(self):
-        return type(self)(
-            arg_defs=set(self.arg_defs),
-            local_defs=set(self.local_defs),
-            uses_free=set(self.uses_free),
-            uses_local=set(self.uses_local),
-            nonlocals=set(self.nonlocals),
-            globals=set(self.globals),
-        )
-
-    replace = dataclasses.replace
-
-
-class VariableAnalysis(NodeVisitor):
-    def inner(self, node: ast.FunctionDef, context: Variables):
-        for arg in node.args.args:
-            context.define_argument(arg.arg)
-        for arg in node.args.kwonlyargs:
-            context.define(arg.arg)
-        if node.args.vararg:
-            context.define(node.args.vararg.arg)
-        if node.args.kwarg:
-            context.define(node.args.kwarg.arg)
-        self(node.body, context)
-        return context
-
-    def __call__(self, node: ast.Name, context: Variables):
-        match node.ctx:
-            case ast.Load():
-                context.use(node.id)
-            case ast.Store():
-                context.define(node.id)
-                context.use(node.id)
-            case _:
-                raise Exception(f"Unsupported context: {node.ctx}")
-        return context
-
-    def __call__(self, node: ast.Nonlocal, context: Variables):
-        for name in node.names:
-            context.declare_nonlocal(name)
-        return context
-
-    def __call__(self, node: ast.Global, context: Variables):
-        for name in node.names:
-            context.declare_global(name)
-        return context
-
-    def __call__(self, node: ast.FunctionDef, context: Variables):
-        context.define(node.name)
-        inner_context = self.inner(node, Variables())
-        for expr in node.args.defaults:
-            recurse(expr, context)
-        for var in inner_context.uses_free:
-            if var in inner_context.globals:
-                context.uses_free.add(var)
-            else:
-                context.use(var)
-        return context
-
-    def reduce(self, node, results, context):
-        for _, x in results:
-            if isinstance(x, Variables):
-                return x
-        return Variables()
 
 
 class SplitTagger(NodeVisitor):
@@ -219,6 +103,7 @@ class Collapse(NodeVisitor):
 
 @dataclass
 class SplitState:
+    name: str = "_func"
     count: object = field(default_factory=count)
     continuation: ast.AST = None
     definitions: dict = field(default_factory=dict)
@@ -231,9 +116,8 @@ class SplitState:
     replace = dataclasses.replace
 
 
-def _encapsulate(args, body, context, cont_name=None):
-    if cont_name is None:
-        cont_name = f"F{context.gensym()}"
+def _encapsulate(args, body, context, cont_name):
+    assert cont_name not in context.definitions
     context.definitions[cont_name] = ast.FunctionDef(
         name=cont_name,
         args=args
@@ -272,11 +156,15 @@ class BodySplitter:
         )
         new_defs = acc_vars.local_defs - upper_vars.local_defs
         to_pass = list(acc_vars.uses_local - new_defs)
+        to_pass.sort()
         args = [ast.Name(id=var, ctx=ast.Load()) for var in to_pass]
         to_pass.append(name)
-        cont_name = _encapsulate(to_pass, self.acc, context)
+
+        cont_name = context.strategy.identify(q, self.acc, name, context)
+        cont_name = _encapsulate(to_pass, self.acc, context, cont_name=cont_name)
+
         cont_struct = ast.Call(
-            func=ast.Name(id="__Bite", ctx=ast.Load()),
+            func=ast.Name(id="__FunBite", ctx=ast.Load()),
             args=[ast.Name(id=cont_name, ctx=ast.Load()), *args],
             keywords=[],
         )
@@ -381,6 +269,7 @@ def split_body(body, context, prebody=[]):
 class Splitter(NodeTransformer):
     def __call__(self, node: ast.FunctionDef, context: SplitState):
         context = SplitState(
+            name=context.name,
             variables=VariableAnalysis().inner(node, Variables()),
             strategy=context.strategy,
         )
@@ -400,14 +289,15 @@ def split(strategy_class):
         fdef = tree.body[0]
         strategy = strategy_class(fn.__globals__)
         SplitTagger.run(fdef, strategy=strategy)
-        fdef = Splitter.run(fdef, context=SplitState(strategy=strategy))
+        fdef = Splitter.run(fdef, context=SplitState(strategy=strategy, name=fn.__name__))
         if isinstance(fdef, list):
             tree.body[:] = fdef
         else:
             tree.body[0] = fdef
         tree = ast.fix_missing_locations(tree)
-        fn.__globals__.update({"__Bite": Bite})
-        exec(compile(tree, "<string>", "exec"), fn.__globals__)
+        tree = ast.increment_lineno(tree, fn.__code__.co_firstlineno - 1)
+        fn.__globals__.update({"__FunBite": FunBite})
+        exec(compile(tree, fn.__code__.co_filename, "exec"), fn.__globals__)
         return strategy.wrap(fn.__globals__[fn.__name__])
 
     return deco
